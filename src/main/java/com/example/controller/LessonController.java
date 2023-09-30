31b6774af2b04a2c61b478f53fe0ef1a60ad22b3
Nishida Tomoya <nishitomo7396@gmail.com>
2023-09-30 23:09:49 +0900

    lesson02実装完了

---
+++ src/main/java/com/example/controller/LessonController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/lesson")
+public class LessonController {
+	
+	@GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World!";
+    }
+	
+	@GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Good Evening!";
+    }
+	
+    @GetMapping("/test/{num}")
+	@ResponseBody
+	public String test(@PathVariable Integer num) {
+        return "引数の値 :" + num;
+    }
+	
+}
