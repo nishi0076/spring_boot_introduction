db3889122709e2520f7b62ab5fb49376905d6d07
Nishida Tomoya <nishitomo7396@gmail.com>
2023-10-03 12:52:09 +0900

    lesson08実装完了

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.entity.Employee;
+import com.example.service.EmployeeService;
+
+@Controller
+@RequestMapping("employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+    	this.employeeService = employeeService;
+    }
+    
+    // データ全件取得
+    @GetMapping("/list")
+    public String showList(Model model) {
+    	List<Employee> employee = this.employeeService.findAllEmployee();
+    	model.addAttribute("employee", employee);
+    	return "employee/list";
+    }
+    
+    
+    // データ1件取得
+    @GetMapping("/find/{employeeId}")
+    public String showEmployee(@PathVariable Integer employeeId, Model model) {
+        Employee employee = this.employeeService.findEmployee(employeeId);
+        model.addAttribute("employee", employee);
+        return "employee/data";
+    }
+    
+    
+    // 絞り込み検索
+    @GetMapping("/searchByName/{name}")
+    public String searchEmployee(@PathVariable String name, Model model) {
+        List<Employee> employees = this.employeeService.findByName(name);
+        model.addAttribute("employees", employees);
+        return "employee/list";
+    }
+}
--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Employee;
 import com.example.repository.EmployeeRepository;
 
 @Service
@@ -14,5 +18,25 @@ public class EmployeeService {
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+    
+    // データ全件取得
+    public List<Employee> findAllEmployee() {
+    	return this.employeeRepository.findAll();
+    }
+    
+    // データ1件取得
+    public Employee findEmployee(Integer employeeId) {
+    	// データ取得
+        Optional<Employee> optionalEmployee = this.employeeRepository.findById(employeeId);
+        // OptionalからEntityクラスの取得
+        Employee employee = optionalEmployee.get();
+        return employee;
+    }
+    
+    
+    // 絞り込み検索
+    public List<Employee> findByName(String name) {
+        return this.employeeRepository.findByName(name);
     }
 }
